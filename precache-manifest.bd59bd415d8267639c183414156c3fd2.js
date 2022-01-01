64706c2db218bd9f73f69e40b597c66bcf3d60ee
Guilherme Santiago <guilherme.santiago95@gmail.com>
2022-01-01 19:35:07 -0300

    Updates

---
+++ precache-manifest.bd59bd415d8267639c183414156c3fd2.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73da7f45b9605ff2aff8bf944406139",
+    "url": "/interactiva-unit-circle/index.html"
+  },
+  {
+    "revision": "dcb228c299e188605647",
+    "url": "/interactiva-unit-circle/static/js/main.1e581125.chunk.js"
+  },
+  {
+    "revision": "52c73ab59f1a51a46c76",
+    "url": "/interactiva-unit-circle/static/js/runtime~main.f06057c5.js"
+  }
+]);
